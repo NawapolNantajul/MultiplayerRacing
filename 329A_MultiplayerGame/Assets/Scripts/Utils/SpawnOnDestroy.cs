a6bea01951aec049b8f14f18ccec689cece5021d
NawapolNantajul <nawapol_nantajul@hotmail.com>
2025-05-08 02:05:13 +0700

    Re-add missing LFS files

---
+++ 329A_MultiplayerGame/Assets/Scripts/Utils/SpawnOnDestroy.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class SpawnOnDestroy : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+
+    private void OnDestroy()
+    {
+        if(!gameObject.scene.isLoaded) {return;}
+        Instantiate(prefab, transform.position, Quaternion.identity);
+    }
+}
